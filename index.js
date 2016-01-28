8dd2fe5c2501a3fe9252ce3982b5dc45916043b1
felixyuebin <felixyuebin@gmail.com>
2016-01-29 00:52:55 +0800

    feat(index.js): add pre and pub intro; add .xgconfig file check
    when user in a folder without .xgconfig will throw an error

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ fis.require.prefixes.unshift('xg');
 fis.cli.name = 'xg';
 fis.cli.info = require('./package.json');
 
-fis.cli.version = function() {
-    logo.logLogo();
-    console.log('  v' + fis.cli.info.version + '\n');
-};
-
 // 读取目录下.xgconfig文件
 var xgconfigPath = path.resolve(process.cwd(), '.xgconfig');
 var xgconfig = {};
@@ -28,8 +23,35 @@ if (fs.existsSync(xgconfigPath)) {
 
 // 确定项目类型，默认为angular
 if (!xgconfig.type || !xgconfig.type in XG_CONFIGS) {
-    xgconfig.type = 'angular';
+    xgconfig.type = null;
 }
 
-// 初始化配置文件
-XG_CONFIGS[xgconfig.type]();
+// 版本信息: xg -v
+fis.cli.version = function () {
+    logo.logLogo();
+    console.log('  v' + fis.cli.info.version + '\n');
+};
+
+if (xgconfig.type) {
+
+    // 初始化配置文件
+    XG_CONFIGS[xgconfig.type]();
+
+    // release help信息: xg release -h
+    var release = fis.require('command', 'release');
+    release.name += '\n\n Media name'
+        + '\n   pre:                      release to xg-server with optimize for finally confirmation'
+        + '\n   pub:                      release to ../dist with optimize for online usage';
+}
+
+
+// release 配置文件监测
+release.fisRun = release.run;
+release.run = function (argv, cli, env) {
+    if (!argv.h && !argv.help && !xgconfig.type) {
+        fis.log.error('Release must run in the folder containing a .xgconfig file!');
+        return ;
+    }
+
+    release.fisRun.apply(release, arguments);
+};
